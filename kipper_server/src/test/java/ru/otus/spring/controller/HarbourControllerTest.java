47fb197b68fa8c9ad3af21780a30b28e134b93d4
andrei.koshelap <andrei.koshelap@kuehne-nagel.com>
2021-05-18 23:18:43 +0300

    [spring20] - added  controller test

--- kipper_server/src/test/java/ru/otus/spring/controller/HarbourControllerTest.java
+++ kipper_server/src/test/java/ru/otus/spring/controller/HarbourControllerTest.java
@@ -1,30 +1,31 @@
 package ru.otus.spring.controller;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.reactive.server.WebTestClient;
-import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.reactive.function.server.RouterFunction;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class ShipControllerTest {
+public class HarbourControllerTest {
 
     @Autowired
     private RouterFunction route;
 
-    @Autowired
-    private WebClient webClient;
 
-    WebTestClient client = WebTestClient
-            .bindToRouterFunction(route)
-            .build();
+    @Test
+    public void testHarbourRoute() {
+        WebTestClient client = WebTestClient
+                .bindToRouterFunction(route)
+                .build();
 
-//        client.get()
-//                .uri("/func/person")
-//                .exchange()
-//                .expectStatus()
-//                .isOk();
+        client.get()
+                .uri("/func/harbour?name=aa")
+                .exchange()
+                .expectStatus()
+                .isOk();
+    }
 }
